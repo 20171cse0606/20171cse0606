febca310133d246688bdda57bfc247791f22c948
20171cse0606 <60249547+20171cse0606@users.noreply.github.com>
2020-01-24 13:36:27 +0530

    Update area.c

--- area.c
+++ area.c
@@ -1,12 +1,10 @@
 /* program to find and print area of circle*/
 #include<stdio.h>
 #define pi 3.141
-cccc
 void main(){
 	float r,area;
 	printf("Enter the radius\n");
 	scanf("%f", &r);
 	area=pi*r*r;
-sss
 	printf("Area(%f)=%f\n",r,area);
 }
